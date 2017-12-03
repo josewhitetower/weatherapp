c471be0b8bbfca0fd1ed503d2ce546c4581d0277
Jose Torreblanca <josewhitetower@gmail.com>
2017-12-03 22:11:18 +0100

    Added promise and polyfill

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import Vue from "vue";
 import App from "./App";
 import ElementUI from "element-ui";
 import "./assets/index.css";
+import "whatwg-fetch";
+import Promise from "promise-polyfill";
+
+// To add to window
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 // import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
